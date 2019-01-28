a7b621292e9cf2c27a61e75e8906e37c508fd2ed
Noah Flint <noah.flint24@gmail.com>
2019-01-28 16:29:38 -0700

    created books index feature test
    Co-authored-by: Peregrine Balas <39976770+PeregrineReed@users.noreply.github.com>

---
+++ spec/features/book_index_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'when visitor visits book index', type: :feature do
+  it 'can see all books' do
+    book_1 = Book.create(title: "100 Pictures Of Spiders", author: "Peregrine", pages: 100, published: 2000, cover: "image here")
+    book_2 = Book.create(title: "What's New Pussy Cat", author: "Noah", pages: 200, published: 1999, cover: "image here")
+
+    visit '/books'
+
+    expect(page).to have_content("Books")
+    expect(page).to have_content(book_1.title)
+    expect(page).to have_content(book_2.title)
+    expect(page).to have_content(book_1.author)
+    expect(page).to have_content(book_2.author)
+    expect(page).to have_content(book_1.pages)
+    expect(page).to have_content(book_2.pages)
+    expect(page).to have_content(book_1.published)
+    expect(page).to have_content(book_2.published)
+    # expect(page).to have_xpath(book_1.cover)
+    # expect(page).to have_xpath(book_2.cover)
+  end
+end
