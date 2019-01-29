9f24fcd6ee7b25dc9dee3f98e3629cc885564d49
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-28 19:26:44 -0700

    Created test for book show page
    Co-authored-by: Noah Flint <34421236+n-flint@users.noreply.github.com>

---
+++ spec/features/show_book_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'when visitor visits book index', type: :feature do
+  it 'can see a single book' do
+    book_1 = Book.create(title: "100 Pictures Of Spiders", author: "Peregrine", pages: 100, published: 2000)
+    book_2 = Book.create(title: "What's New Pussy Cat", author: "Noah", pages: 200, published: 1999)
+
+    visit "/books/#{book_1.id}"
+
+    expect(page).to have_content("Books")
+    expect(page).to have_content("#{book_1.title}")
+    expect(page).to have_content("Author: #{book_1.author}")
+    expect(page).to have_content("Page Count: #{book_1.pages}")
+    expect(page).to have_content("Publication Date: #{book_1.published}")
+    # expect(page).to have_xpath(book_1.cover)
+
+    expect(page).to_not have_content("#{book_2.title}")
+    expect(page).to_not have_content("Author: #{book_2.author}")
+    expect(page).to_not have_content("Page Count: #{book_2.pages}")
+    expect(page).to_not have_content("Publication Date: #{book_2.published}")
+    # expect(page).to have_xpath(book_2.cover)
+  end
+end
