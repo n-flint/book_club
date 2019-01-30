42bcd36f2c5bfaff7cf2cbfb2f2e28da321be8b0
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-30 14:50:44 -0700

    Added review link, debugged book feature tests
    Co-authored-by: Noah Flint <34421236+n-flint@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190130204638) do
+ActiveRecord::Schema.define(version: 20190130211556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,14 @@ ActiveRecord::Schema.define(version: 20190130204638) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "cover"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.text "review"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "book_authors", "authors"
