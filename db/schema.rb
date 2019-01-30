cf3f39a7670e486faaa07d75f6b4f238554e845a
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-30 13:51:23 -0700

    Added covers to books
    Co-authored-by: Noah Flint <34421236+n-flint@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190130022953) do
+ActiveRecord::Schema.define(version: 20190130204638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20190130022953) do
     t.integer "published"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "cover"
   end
 
   add_foreign_key "book_authors", "authors"
--- spec/features/book_index_spec.rb
+++ spec/features/book_index_spec.rb
@@ -2,21 +2,25 @@ require 'rails_helper'
 
 describe 'when visitor visits book index', type: :feature do
   it 'can see all books' do
-    book_1 = Book.create(title: "100 Pictures Of Spiders", author: "Peregrine", pages: 100, published: 2000)
-    book_2 = Book.create(title: "What's New Pussy Cat", author: "Noah", pages: 200, published: 1999)
+    cover = "https://cdn4.iconfinder.com/data/icons/basic-17/80/22_BO_open_book-512.png"
+    book_1 = Book.create(title: "100 Pictures Of Spiders", author: "Peregrine", pages: 100, published: 2000, cover: cover)
+    book_2 = Book.create(title: "What's New Pussy Cat", author: "Noah", pages: 200, published: 1999, cover: cover)
 
     visit books_path
 
+    save_and_open_page
+
     expect(page).to have_content("Books")
     expect(page).to have_content(book_1.title)
-    expect(page).to have_content(book_2.title)
     expect(page).to have_content(book_1.author)
-    expect(page).to have_content(book_2.author)
     expect(page).to have_content(book_1.pages)
-    expect(page).to have_content(book_2.pages)
     expect(page).to have_content(book_1.published)
-    expect(page).to have_content(book_2.published)
     # expect(page).to have_xpath(book_1.cover)
+
+    expect(page).to have_content(book_2.title)
+    expect(page).to have_content(book_2.author)
+    expect(page).to have_content(book_2.pages)
+    expect(page).to have_content(book_2.published)
     # expect(page).to have_xpath(book_2.cover)
   end
 end
