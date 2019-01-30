468db78c481e87a9420c404608d3a2ef6916a63c
Noah Flint <noah.flint24@gmail.com>
2019-01-29 19:38:50 -0700

    created book_author table, and relationships
    Co-authored-by: Peregrine Balas <39976770+PeregrineReed@users.noreply.github.com>

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
 describe Book, type: :model do
+
+  describe "relationships" do
+    it {should have_many(:authors)}
+  end
+
   describe "validations" do
     it {should validate_presence_of(:title)}
     it {should validate_presence_of(:author)}
     it {should validate_presence_of(:pages)}
     it {should validate_presence_of(:published)}
+  end
+
+  describe "class methods" do
+  end
+
+  describe "instance methods" do
   end
 end
