8234268297327bb379cbe0cc2327dfc954ed181c
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-02-02 16:55:46 -0700

    Added create book route
    Co-authored-by: Noah Flint <34421236+n-flint@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #what does shallow: true do?
-  resources :books, shallow: true, only: [:index, :show, :new] do
+  resources :books, shallow: true, only: [:index, :show, :new, :create] do
     resources :reviews, only: [:create, :new]
     resources :authors, only: :show
   end
