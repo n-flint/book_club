6e1832b3c655f0a43b20e12a9e558cde62fa1465
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-02-02 17:06:45 -0700

    Added test for titleizing author names
    Co-authored-by: Noah Flint <34421236+n-flint@users.noreply.github.com>

--- spec/features/new_book_spec.rb
+++ spec/features/new_book_spec.rb
@@ -25,8 +25,24 @@ RSpec.describe 'when visitor visits new book page', type: :feature do
     click_button "Add Book"
 
     book = Book.all.first
-    
+
     expect(current_path).to eq(book_path(book))
+  end
+
+  it 'titleizes author names' do
+
+    visit new_book_path
+
+    fill_in "book_title", with: "Dog Wizards"
+    fill_in "book_author", with: "tim allen, noah flint"
+    fill_in "book_pages", with: 320
+    fill_in "book_published", with: 2019
+
+    click_button "Add Book"
+
+    #TODO change have_content to have_link
+    expect(page).to have_content("Tim Allen")
+    expect(page).to have_content("Noah Flint")
   end
 
 end
