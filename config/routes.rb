cf58abaa15b46a0773cf1e0a32dbe9c77dec72b1
Noah Flint <noah.flint24@gmail.com>
2019-02-04 15:02:34 -0700

    add root route to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root "welcome#index"
   #what does shallow: true do?
   resources :books, shallow: true, only: [:index, :show, :new, :create] do
     resources :reviews, only: [:create, :new]
