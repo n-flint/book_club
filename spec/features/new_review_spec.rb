87ad8d8bf0a5354ff39c940ca2c4d5f872f1a3e6
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-02-05 08:46:41 -0700

    Tested for user names as links and to be titleized

--- spec/features/new_review_spec.rb
+++ spec/features/new_review_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe 'when visitor visits new review', type: :feature do
     expect(page).to have_content("Rating")
     expect(page).to have_content("Review")
     expect(page).to have_button("Create Review")
-    expect(page).to have_css("img[src*='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSd3cgsv8lMoNU4g8dDN1hUqKlXAR3DTITUd5rl1tMuYds_wAP6']")
+    expect(page).to have_css("img[src*='#{@book_1.cover}']")
   end
 
   it 'stays on page if form is without a review' do
@@ -40,7 +40,7 @@ RSpec.describe 'when visitor visits new review', type: :feature do
     expect(page).to have_content("Rating")
     expect(page).to have_content("Review")
     expect(page).to have_button("Create Review")
-    expect(page).to have_css("img[src*='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSd3cgsv8lMoNU4g8dDN1hUqKlXAR3DTITUd5rl1tMuYds_wAP6']")
+    expect(page).to have_css("img[src*='#{@book_1.cover}']")
   end
 
   it 'stays on page if form is without a user' do
@@ -59,7 +59,7 @@ RSpec.describe 'when visitor visits new review', type: :feature do
     expect(page).to have_content("Rating")
     expect(page).to have_content("Review")
     expect(page).to have_button("Create Review")
-    expect(page).to have_css("img[src*='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSd3cgsv8lMoNU4g8dDN1hUqKlXAR3DTITUd5rl1tMuYds_wAP6']")
+    expect(page).to have_css("img[src*='#{@book_1.cover}']")
   end
 
   it 'stays on page if form is without a rating' do
@@ -78,7 +78,7 @@ RSpec.describe 'when visitor visits new review', type: :feature do
     expect(page).to have_content("Rating")
     expect(page).to have_content("Review")
     expect(page).to have_button("Create Review")
-    expect(page).to have_css("img[src*='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSd3cgsv8lMoNU4g8dDN1hUqKlXAR3DTITUd5rl1tMuYds_wAP6']")
+    expect(page).to have_css("img[src*='#{@book_1.cover}']")
   end
 
   it 'stays on page if form is without a title' do
@@ -97,7 +97,7 @@ RSpec.describe 'when visitor visits new review', type: :feature do
     expect(page).to have_content("Rating")
     expect(page).to have_content("Review")
     expect(page).to have_button("Create Review")
-    expect(page).to have_css("img[src*='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSd3cgsv8lMoNU4g8dDN1hUqKlXAR3DTITUd5rl1tMuYds_wAP6']")
+    expect(page).to have_css("img[src*='#{@book_1.cover}']")
   end
 
   it 'redirects to book show page if form is saved' do
@@ -112,6 +112,35 @@ RSpec.describe 'when visitor visits new review', type: :feature do
     click_button "Create Review"
 
     expect(current_path).to eq(book_path(@book_1))
+
+    review_id = Review.find_by(title: "Best Book Ever").id
+    within "#review-#{review_id}" do
+      expect(page).to have_link("Tim Allen")
+    end
+  end
+
+  it 'titleizes user names and converts them to links to user show page' do
+
+    visit new_book_review_path(@book_1)
+
+    fill_in "review_title", with: "Best Book Ever"
+    fill_in "review_user", with: "tim allen"
+    fill_in "review_rating", with: 5
+    fill_in "review_review", with: "This is a must read. I loved every sentence!"
+
+    click_button "Create Review"
+
+    expect(current_path).to eq(book_path(@book_1))
+
+    user = User.find_by(name: "Tim Allen")
+    review_id = Review.find_by(title: "Best Book Ever").id
+
+    within "#review-#{review_id}" do
+      expect(page).to have_link("Tim Allen")
+      click_link("Tim Allen")
+    end
+
+    expect(current_path).to eq(user_path(user))
   end
 
   it 'sees a nav bar' do
