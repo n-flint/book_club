6ff42ff4331eb815067f93595b05810a2e4c07f9
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-02-05 11:40:40 -0700

    Added validation for book_id

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :user_id
+  validates_presence_of :book_id
   validates_presence_of :rating
   validates_presence_of :review
   belongs_to :user
