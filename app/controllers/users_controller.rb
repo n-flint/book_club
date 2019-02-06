85d68c3132a4fd9f35d248b1851066431bd0e664
Noah Flint <noah.flint24@gmail.com>
2019-02-05 18:23:26 -0700

    app/views/users/show.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     if params[:sort] == "n_to_o"
+      @user.reviews = @user.reviews.order(created_at: :asc)
+    else
+    params[:sort] == "n_to_o"
       @user.reviews = @user.reviews.order(created_at: :desc)
     end
-    
+
   end
 end
